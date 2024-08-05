300817b64966efba65bc7038a94f8f424d4e6bde
CPLjynxedCreations <CPLjynxedCreations@outlook.com>
2024-08-05 14:17:47 +1000

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace NewTillSystem
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
