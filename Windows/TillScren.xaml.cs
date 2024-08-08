8d9caa9061f9531d043d8d716dadb2bb8c2827b0
CPLjynxedCreations <CPLjynxedCreations@outlook.com>
2024-08-09 01:51:42 +1000

    Clearing strings with for loops!!!!! GOT IT!!

--- Windows/TillScren.xaml.cs
+++ Windows/TillScren.xaml.cs
@@ -3,6 +3,7 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -16,7 +17,6 @@ namespace NewTillSystem
     public partial class TillScren : Window
     {
         private string strEmpty = "";
-        private int saleScreeenLine;
 
         public TillScren()
         {
@@ -26,56 +26,36 @@ namespace NewTillSystem
 
         private void ClearStartStrings()
         {
-            var builder = new StringBuilder();
-            for (int i = 1; i <= 13; i++)
+
+            for (int i = 1; i <= 15; i++)
             {
-                string btnChange = "btnRow";
-                
-                builder.Append(btnChange + i);
-                Debug.WriteLine(btnChange + i);
+                string btnRow = "btnRow" + i;
+                string lbl1Row = "lbl1Row" + i;
+                string lbl2Row = "lbl2Row" + i;
+                foreach (UIElement item in grSaleScreen.Children)
+                {
+                    if (item.GetType() == typeof(ToggleButton))
+                    {
+                        ToggleButton togButton = (ToggleButton)item;
+                        if (togButton.Name == btnRow)
+                        {
+                            togButton.Content = strEmpty;
+                        }
+                    }
+                    if (item.GetType() == typeof(TextBlock))
+                    {
+                        TextBlock txtBlock = (TextBlock)item;
+                        if (txtBlock.Name == lbl1Row)
+                        {
+                            txtBlock.Text = strEmpty;
+                        }
+                        if (txtBlock.Name == lbl2Row)
+                        {
+                            txtBlock.Text = strEmpty;
+                        }
+                    }
+                }
             }
-            
-            btnRow1.Content = strEmpty;
-            btnRow2.Content = strEmpty;
-            btnRow3.Content = strEmpty;
-            btnRow4.Content = strEmpty;
-            btnRow5.Content = strEmpty;
-            btnRow6.Content = strEmpty;
-            btnRow7.Content = strEmpty;
-            btnRow8.Content = strEmpty;
-            btnRow9.Content = strEmpty;
-            btnRow10.Content = strEmpty;
-            btnRow11.Content = strEmpty;
-            btnRow12.Content = strEmpty;
-            btnRow13.Content = strEmpty;
-            btnRow13.Content = strEmpty;
-            lbl1Row1.Text = strEmpty;
-            lbl2Row1.Text = strEmpty;
-            lbl1Row2.Text = strEmpty;
-            lbl2Row2.Text = strEmpty;
-            lbl1Row3.Text = strEmpty;
-            lbl2Row3.Text = strEmpty;
-            lbl1Row4.Text = strEmpty;
-            lbl2Row4.Text = strEmpty;
-            lbl1Row5.Text = strEmpty;
-            lbl2Row5.Text = strEmpty;
-            lbl1Row6.Text = strEmpty;
-            lbl2Row6.Text = strEmpty;
-            lbl1Row7.Text = strEmpty;
-            lbl2Row7.Text = strEmpty;
-            lbl1Row8.Text = strEmpty;
-            lbl2Row8.Text = strEmpty;
-            lbl1Row9.Text = strEmpty;
-            lbl2Row9.Text = strEmpty;
-            lbl1Row10.Text = strEmpty;
-            lbl2Row10.Text = strEmpty;
-            lbl1Row11.Text = strEmpty;
-            lbl2Row11.Text = strEmpty;
-            lbl1Row12.Text = strEmpty;
-            lbl2Row12.Text = strEmpty;
-            lbl1Row13.Text = strEmpty;
-            lbl2Row13.Text = strEmpty;
-            lblSaleScreenTotalAmount.Text = strEmpty;
         }
     }
 }
