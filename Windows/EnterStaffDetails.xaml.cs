2cf26d517a25e688f2aad532be6dbeb7dfa505d7
CPLjynxedCreations <CPLjynxedCreations@outlook.com>
2024-08-12 19:18:53 +1000

    moves staff to edit to create staff

--- Windows/EnterStaffDetails.xaml.cs
+++ Windows/EnterStaffDetails.xaml.cs
@@ -1,5 +1,7 @@
+using ClosedXML.Excel;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,31 +16,94 @@ using System.Windows.Shapes;
 
 namespace NewTillSystem.Windows
 {
-    /// <summary>
-    /// Interaction logic for EnterStaffDetails.xaml
-    /// </summary>
     public partial class EnterStaffDetails : Window
     {
+        private string strSetStaffName;
+        private string strXlsxStaffNameColumn = "A";
+        private string strSetStaffLastName;
+        private string strXlsxStaffLastNameColumn = "B";
+        private string strSetStaffID;
+        private string strXlsxStaffIDColumn = "C";
+        private string strSetStaffRole;
+        private string strXlsxStaffRollColumn = "D";
+        private string strAdmin = "ADMIN";
+        private string strManager = "MANAGER";
+        private string strStaff = "STAFF";
+
         public EnterStaffDetails()
         {
             InitializeComponent();
-            //set staff list for combobox from excel ();
+            GetCurrentStaffList();
         }
 
         //set staff()
+        private void GetCurrentStaffList()
+        {
+            var workBook = new XLWorkbook("C:\\Users\\Cpljy\\source\\repos\\Projects\\NewTillSystem\\Resources\\StaffID.xlsx");
+            var workSheet = workBook.Worksheet("Staff");
+            int intStaffAmount = workSheet.LastRowUsed().RowNumber();
+            for (int i = 1; i <= intStaffAmount; i++)
+            {
+                var readXlsxDataStaffName = workSheet.Cell(i, strXlsxStaffNameColumn).GetValue<string>();
+                var readXlsxDataStaffLastName = workSheet.Cell(i, strXlsxStaffLastNameColumn).GetValue<string>();
+                var readXlsxDataStaffID = workSheet.Cell(i, strXlsxStaffIDColumn).GetValue<string>();
+                var readXlsxDataStaffRole = workSheet.Cell(i, strXlsxStaffRollColumn).GetValue<string>();
+                boxSelectExistingStaff.Items.Add(readXlsxDataStaffName + " " + readXlsxDataStaffLastName);
+                var joinNames = readXlsxDataStaffName + " " + readXlsxDataStaffLastName;
+                if(boxSelectExistingStaff.Text == joinNames)
+                {
+                    string[] strSeperateNames = joinNames.Split(' ');
+                    strSetStaffName = strSeperateNames[0];
+                    strSetStaffLastName = strSeperateNames[1];
+                    strSetStaffID = readXlsxDataStaffID;
+                    strSetStaffRole = readXlsxDataStaffRole;
+                }
+            }
+            workSheet.ColumnsUsed().AdjustToContents();
+            workBook.Save();
+        }
 
         //delete click remove from staff();
-            //delete off excel file -> name -> ID -> role
-            //save
+        //delete off excel file -> name -> ID -> role
+        //save
+        private void DeleteCurrentStaff()
+        {
+
+        }
 
         //edit move to left side to enter datails();
-            //bool edit staff
-            //save staff name to string edit details code names roll
-            //dispay in left in all boxes
-            //if edit ok will override that staff member
+        //bool edit staff
+        //save staff name to string edit details code names roll
+        //dispay in left in all boxes
+        //if edit ok will override that staff member
+        private void btnEditStaff_Click(object sender, RoutedEventArgs e)
+        {
+            boxSelectRole.Items.Remove(boxSelectRole);
+            GetCurrentStaffList();
+            txtEnterStaffFirstName.Text = strSetStaffName;
+            txtEnterStaffLastName.Text = strSetStaffLastName;
+            txtEnterStaffPin.Text = strSetStaffID;
+            if(strSetStaffRole == strAdmin)
+            {
+                boxSelectRole.SelectedItem = boxRoleSelectAdmin;
+            }
+            else if (strSetStaffRole == strManager)
+            {
+                boxSelectRole.SelectedItem = boxRoleSelectManager;
+            }
+            else if (strSetStaffRole == strAdmin)
+            {
+                boxSelectRole.SelectedItem = boxRoleSelectStaff;
+            }
+
+        }
 
         //on ok check file if doesnt exist close();
-            // see if this saves details without close in main
+        // see if this saves details without close in main
         //else exists try again prompt
+        private void AddStaff()
+        {
+
+        }
     }
 }
