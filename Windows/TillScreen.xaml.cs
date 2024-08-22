2c7f52febd70b8e7c635def51ea58203dfa57fd6
CPLjynxedCreations <CPLjynxedCreations@outlook.com>
2024-08-23 02:10:32 +1000

    Now has edit product from manage once done open manage and press stop edit

--- Windows/TillScreen.xaml.cs
+++ Windows/TillScreen.xaml.cs
@@ -29,8 +29,6 @@ namespace NewTillSystem
         private int intProductButtonCount = 51;
         private bool boolIsMinus;
 
-        private bool boolCanEditProduct;
-        private bool boolEditStaff;
 
         private int intXlsxProductRow;
         private string strXlsxProductColumn = "A";
@@ -46,6 +44,8 @@ namespace NewTillSystem
         private bool boolIsAdmin;
         private bool boolIsManager;
         private bool boolIsStaff;
+        private bool boolCanEditProduct;
+        private bool boolEditStaff;
 
         public string currentDate;
         public string currentTime;
@@ -59,7 +59,7 @@ namespace NewTillSystem
             SetProductButtonDetails();
 
             lblSaleScreenDate.Dispatcher.InvokeAsync(GetDateTime, DispatcherPriority.SystemIdle);
-            
+
 
 
             TillLogOn();
@@ -170,9 +170,9 @@ namespace NewTillSystem
                 workSheet.Cell(intXlsxProductRow, strXlsxProductColumn).Value = openPrompt.strProductName;
                 workSheet.Cell(intXlsxProductRow, strXlsxPriceColumn).Value = openPrompt.strProductPrice;
                 workBook.Save();
-                boolCanEditProduct = false;
                 //MAKE IT SO EDIT PRODUCT MANAGE CHANGES TO TURN EDIT OFF
             }
+            //ADD TO SALE SCREEN WHEN MADE
         }
 
         private void btnAdminLogOut_Click(object sender, EventArgs e)
@@ -190,8 +190,24 @@ namespace NewTillSystem
                 ManageTillWindow openManageWindow = new ManageTillWindow();
                 openManageWindow.Owner = Application.Current.MainWindow;
                 openManageWindow.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+                if (boolCanEditProduct)
+                {
+                    openManageWindow.btnManageEditProducts.Content = "Stop Product Edit";
+                }
                 openManageWindow.btnManageClose.Click += (sender, e) => { openManageWindow.Close(); };
-                openManageWindow.btnManageEditProducts.Click += (sender, e) => { openManageWindow.Close(); boolCanEditProduct = true; };
+                openManageWindow.btnManageEditProducts.Click += (sender, e) =>
+                {
+                    if (!boolCanEditProduct)
+                    {
+                        boolCanEditProduct = true;
+                    }
+                    else
+                    {
+                        boolCanEditProduct = false;
+                    }
+                    openManageWindow.Close();
+                };
+                //MAKE BUTTON TO STOP EDIT POP UP
                 openManageWindow.btnManageEditStaff.Click += (sender, e) => { openManageWindow.Close(); ManageEditStaff(); };
                 openManageWindow.ShowDialog();
             }
@@ -203,11 +219,7 @@ namespace NewTillSystem
             EnterStaffDetails openEditStaff = new EnterStaffDetails();
             openEditStaff.Owner = Application.Current.MainWindow;
             openEditStaff.WindowStartupLocation = WindowStartupLocation.CenterOwner;
-            //check details
-            //openEditStaff.btnOk.Click += (sender, e) => { openEditStaff.Close(); }; // save entered details
-            //if newstaff then close and save details
-            //if edit and pressed. save new details
-            openEditStaff.btnCancel.Click += (sender, e) => { openEditStaff.Close(); }; // save nothing
+            openEditStaff.btnCancel.Click += (sender, e) => { openEditStaff.Close(); };
             openEditStaff.ShowDialog();
 
         }
